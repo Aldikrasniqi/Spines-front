605d823bf3b6397e043c337f545e9a7cf841867a
altin hysa <altinhysa84@gmail.com>
2024-10-05 16:10:07 +0200

    update base-url of api

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,2 +1,2 @@
 // export const API_URL = 'http://api-dev.spines.tech/api/';
-export const API_URL = 'https://spines-backend-dev-d950a9138db1.herokuapp.com/api/';
+export const API_URL = 'https://spines-be-754fb04a8763.herokuapp.com/api/';
